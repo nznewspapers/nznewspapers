ca5306cff45da54931a227bc8227111fa00495bb
NZNewspapers <41760435+nznewspapers@users.noreply.github.com>
2021-11-03 22:12:32 -0500

    Create summary data for home and "by title" pages.

--- scripts/nzn-summarise.js
+++ scripts/nzn-summarise.js
@@ -18,39 +18,91 @@ console.log("Output dir: " + nznShared.jsonDir);
 function summarise(idList) {
   console.log("Start summarise()");
 
-  // The summary data we will create:
-  titleMap = {};
-  titlePath = path.join(nznShared.jsonDir, "titleInfo.json");
-
   // Read and re-write the newspaper Json:
+  newspaperList = [];
+  placenames = new Set();
   var count = 0;
   var skipped = 0;
 
+  // Extract the newspaper records we're interested in:
   idList.forEach(function (newspaperId) {
-    const newspaper = nznShared.readNewspaper(newspaperId);
+    var newspaper = nznShared.readNewspaper(newspaperId);
 
     if (newspaper && newspaper.genre == "Newspaper") {
       count += 1;
-      titleMap[newspaper.title] = newspaperId;
+      const sortKey = newspaper.title
+        .toLowerCase()
+        .replace(/\W+/g, "")
+        .replace("the", "");
+      newspaper.sortKey = sortKey;
+      newspaper.titleSection =
+        sortKey[0].toUpperCase() + sortKey[0].toLowerCase();
+      newspaperList.push(newspaper);
+      placenames.add(newspaper.placename);
     } else {
       skipped += 1;
     }
   });
 
-  var titleData = {
+  console.log("  Kept summarise(): " + count + " records");
+  console.log("  Skipped summarise(): " + skipped + " records.");
+
+  // Generate data for the "Home" page:
+  var homeInfo = {
     stats: {},
     lists: {},
   };
-  titleData.stats.count = count;
-  var keys = Object.keys(titleMap).sort();
-  keys.forEach(function (key) {
-    listId = key[0].toUpperCase() + key[0].toLowerCase();
-    if (!titleData.lists[listId]) {
-      titleData.lists[listId] = {};
-    }
-    titleData.lists[listId][key] = titleMap[key];
+  homeInfo.stats.count = count;
+  homeInfo.stats.places = placenames.size;
+
+  newspaperList.sort(function (a, b) {
+    return a.placecode.localeCompare(b.placecode);
   });
-  nznShared.writeJsonDict(titleData, titlePath);
+
+  newspaperList.forEach(function (newspaper) {
+    if (!homeInfo.lists[newspaper.region]) {
+      homeInfo.lists[newspaper.region] = {};
+    }
+    if (!homeInfo.lists[newspaper.region][newspaper.placename]) {
+      homeInfo.lists[newspaper.region][newspaper.placename] = 1;
+    } else {
+      homeInfo.lists[newspaper.region][newspaper.placename] += 1;
+    }
+  });
+
+  const homeInfoPath = path.join(nznShared.jsonDir, "homeInfo.json");
+  nznShared.writeJsonDict(homeInfo, homeInfoPath);
+
+  // Generate data for the "By Title" page:
+  var titleInfo = {
+    stats: {},
+    lists: {},
+  };
+  titleInfo.stats.count = count;
+  titleInfo.stats.places = placenames.size;
+
+  newspaperList.sort(function (a, b) {
+    return a.sortKey.localeCompare(b.sortKey);
+  });
+
+  newspaperList.forEach(function (newspaper) {
+    if (!titleInfo.lists[newspaper.titleSection]) {
+      titleInfo.lists[newspaper.titleSection] = [];
+    }
+    var record = {
+      title: newspaper.title,
+      firstYear: newspaper.firstYear,
+      finalYear: newspaper.finalYear,
+      placename: newspaper.region,
+      placename: newspaper.placename,
+      urlCurrent: newspaper.urlCurrent,
+      urlDigitized: newspaper.urlDigitized,
+    };
+    titleInfo.lists[newspaper.titleSection].push(record);
+  });
+
+  const titleInfoPath = path.join(nznShared.jsonDir, "titleInfo.json");
+  nznShared.writeJsonDict(titleInfo, titleInfoPath);
 
   console.log("End summarise(): " + count + " records");
   console.log("End summarise(): " + skipped + " records.");
