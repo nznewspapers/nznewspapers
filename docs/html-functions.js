53111f4eaef5c9cae5e9d95455afcfcbef11af6c
NZNewspapers <41760435+nznewspapers@users.noreply.github.com>
2021-12-29 12:32:03 -0600

    Create one function to describe a newspaper.

--- docs/html-functions.js
+++ docs/html-functions.js
@@ -24,6 +24,69 @@ function appendLink(parent, url, text) {
 }
 
 /**
+ * Translate a MARC "year" into a short phrase, allowing for unknowns like 19uu.
+ * @param {*} year The concise form of the year (may be partially unknown).
+ * @returns A prettier form of the year.
+ */
+function prettyYear(year) {
+  if (year.endsWith("uuuu")) {
+    return "an unknown date";
+  } else if (year.endsWith("uu")) {
+    var century = year.substring(0, 2);
+    return "sometime in the " + century + "00's";
+  } else if (year.endsWith("u")) {
+    var decade = year.substring(0, 3);
+    return "sometime in the " + decade + "0's";
+  } else {
+    return year;
+  }
+}
+
+/**
+ * Append an HTML sentence that describes a newspaper to an HTML element.
+ * @param {} div An element that we're going to add the description too.
+ * @param {*} newspaper An object that holds the newspaper info, including, id, title, placename, etc.
+ */
+function appendNewspaperInfo(div, newspaper) {
+  url = "newspaper.html?id=" + newspaper.id;
+  appendLink(div, url, newspaper.title);
+
+  appendText(div, ", ");
+  if (newspaper.finalYear == 9999) {
+    appendText(div, " since " + prettyYear(newspaper.firstYear));
+  } else {
+    appendText(
+      div,
+      " from " +
+        prettyYear(newspaper.firstYear) +
+        " to " +
+        prettyYear(newspaper.finalYear)
+    );
+  }
+
+  if (newspaper.placename) {
+    appendText(div, ", of ");
+    url = "place.html?place=" + newspaper.placename;
+    appendLink(div, url, newspaper.placename);
+  }
+
+  if (newspaper.urlDigitized || newspaper.urlCurrent) {
+    appendText(div, ", is ");
+  }
+  if (newspaper.urlDigitized) {
+    appendLink(div, newspaper.urlDigitized, "digitised");
+  }
+  if (newspaper.urlDigitized && newspaper.urlCurrent) {
+    appendText(div, " and ");
+  }
+  if (newspaper.urlCurrent) {
+    appendLink(div, newspaper.urlCurrent, "online");
+  }
+
+  appendText(div, ". ");
+}
+
+/**
  * Read a local JSON file to an object and return it.
  * @param {*} url The URL of the JSON file (usually relative to the curren tpage).
  * @returns
