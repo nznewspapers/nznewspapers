0a018ff2131e1fa8fbc29092effe7ad0920dfe1f
NZNewspapers <41760435+nznewspapers@users.noreply.github.com>
2022-07-04 23:53:08 -0500

    Figure out which records need to be added.

--- scripts/nzn-nat-bib-updater.js
+++ scripts/nzn-nat-bib-updater.js
@@ -33,6 +33,7 @@ let marcNumberToNewspaperId = {};
 for (const [key, value] of Object.entries(newspaperRecords)) {
   // Save the title:
   marcNumber = newspaperRecords[key].idMarcControlNumber;
+
   if (!marcNumber) {
     // No MARC number, ignore for now.
     // Often the MARC Control Number refers to a Masthead or related record.
@@ -63,6 +64,25 @@ console.log(
   "Read " + Object.keys(newspaperRecords).length + " newspaper records"
 );
 
+// Gather some stats as we go...
+let stats = {};
+
+function addStats(label) {
+  if (stats[label]) {
+    stats[label] += 1;
+  } else {
+    stats[label] = 1;
+  }
+}
+
+function statsToString() {
+  let str = "Stats\n";
+  for (const [key, value] of Object.entries(stats)) {
+    str += " * " + key + " -> " + value + "\n";
+  }
+  return str;
+}
+
 function readMarcFile(marcFile) {
   // Set up a MARC reader for the NatBib records
   let reader = Marc.stream(fs.createReadStream(marcFile), "Iso2709");
@@ -82,15 +102,6 @@ function readMarcFile(marcFile) {
         serialCounter +
         " / " +
         reader.count
-    );
-    console.log(
-      "Stats: " +
-        countNoNatLibMarc +
-        " missing control number, " +
-        countMatch +
-        " match, " +
-        countNoMatch +
-        " don't match."
     );
   }, 5000);
 
@@ -112,15 +123,20 @@ function readMarcFile(marcFile) {
 
       // General Inormation Record
       let generalInfoValue = null;
-      let continuingResourceType = null;
+      let dateOnFile = null;
+      let typeOfDate = null;
       let date1 = null;
       let date2 = null;
+      let continuingResourceType = null;
       let ff = record.match(/008/, (field) => {
         generalInfoValue = field.value;
-        continuingResourceType = field.value.charAt(21);
+        dateOnFile = field.value.substring(0, 6);
+        typeOfDate = field.value.charAt(6);
         date1 = field.value.substring(7, 11);
         date2 = field.value.substring(11, 15);
+        continuingResourceType = field.value.charAt(21);
       });
+      let isCurrentlyPublished = typeOfDate == "c";
 
       if (continuingResourceType == "n") {
         newspaperCounter += 1;
@@ -142,12 +158,61 @@ function readMarcFile(marcFile) {
           });
         });
 
-        // Title:
+        // Track format:
+        let isElectronicResource = false;
+        let isMicroformResource = false;
+
+        // Title & Medium:
         let title = null;
+        let medium = null;
         record.get(/245/).forEach((field) => {
           field["subf"].forEach((pair) => {
             if (pair[0] == "a") {
               title = pair[1];
+            } else if (pair[0] == "h") {
+              medium = pair[1];
+              if (medium.startsWith("[electronic resource]")) {
+                isElectronicResource = true;
+              } else if (medium.startsWith("[microform]")) {
+                isMicroformResource = true;
+              }
+            }
+          });
+        });
+
+        // Uniform Title:
+        let uniformTitle = null;
+        record.get(/130/).forEach((field) => {
+          field["subf"].forEach((pair) => {
+            if (pair[0] == "a") {
+              uniformTitle = pair[1];
+            }
+          });
+        });
+
+        // Edition:
+        let edition = null;
+        record.get(/250/).forEach((field) => {
+          field["subf"].forEach((pair) => {
+            if (pair[0] == "a") {
+              edition = pair[1];
+            }
+          });
+        });
+
+        // Physical Description / Extent:
+        let physicalExtent = null;
+        record.get(/300/).forEach((field) => {
+          field["subf"].forEach((pair) => {
+            if (pair[0] == "a") {
+              physicalExtent = pair[1];
+              if (physicalExtent.includes("microf")) {
+                isMicroformResource = true;
+              } else if (physicalExtent.includes("online resource")) {
+                isElectronicResource = true;
+              } else if (physicalExtent.includes("electronic documents")) {
+                isElectronicResource = true;
+              }
             }
           });
         });
@@ -172,35 +237,55 @@ function readMarcFile(marcFile) {
           });
         });
 
-        if (newspaperCounter < 5) {
-          console.log("Newspaper Record (" + marcControlNumber + "):");
-
+        if (newspaperCounter < 2) {
+          console.log("Sample Newspaper Record (" + marcControlNumber + "):");
           console.log(" * Leader:     " + record.leader);
-          // console.log(" * Record Type:  " + recordType);
-          // console.log(" * Record Level: " + recordBibLevel);
-          // console.log(" * 008 General Info: " + generalInfoValue);
-          // console.log(" * Resource Type: " + continuingResourceType);
-          // console.log(" * MARC Ctrl:  " + marcControlNumber);
           console.log(" * Title:      " + title);
           console.log(" * Date Range: " + date1 + "-" + date2);
           console.log(" * Genre:      " + genre);
           console.log(" * Placename:  " + placename);
+          console.log("");
         }
 
         // Does this Marc Control Number match a known record?
         if (!marcControlNumber) {
-          countNoNatLibMarc += 1;
+          addStats("count-skpped-no-nz-control-number");
+        } else if (isMicroformResource) {
+          // Ignore records for micrfilm and mocroform materials:
+          addStats("count-skpped-micoform");
+        } else if (isElectronicResource) {
+          // Ignore records for digitised and born-digital materials:
+          addStats("count-skipped-electronic");
         } else if (newspaperIdMatch) {
           // console.log("Match " + marcControlNumber + " -> " + newspaperIdMatch);
+          addStats("count-match-yay");
           countMatch += 1;
         } else {
-          countNoMatch += 1;
-          console.log("Marc has no match (" + countNoMatch + "):");
+          addStats("count-no-match");
+          console.log(
+            "Marc has no match (" + stats["count-no-id-match"] + "):"
+          );
           console.log(" * MARC Ctrl#: " + marcControlNumber);
+          console.log(" * Date added: " + dateOnFile);
+
+          // Last load was: 2013-04-02
+          if (dateOnFile > "130402") {
+            console.log("   * NEW RECORD!!!");
+            addStats("count-no-match-and-new-record");
+          }
+
           console.log(" * Title:      " + title);
+          if (edition) {
+            console.log("   * Edition: " + edition);
+          }
+          if (uniformTitle) {
+            console.log("   * Uniform Title: " + uniformTitle);
+          }
           console.log(" * Date Range: " + date1 + "-" + date2);
+          console.log("   * Current?: " + isCurrentlyPublished);
           console.log(" * Genre:      " + genre);
           console.log(" * Placename:  " + placename);
+          console.log(statsToString());
         }
 
         writers.forEach((writer) => writer.write(record));
@@ -213,23 +298,8 @@ function readMarcFile(marcFile) {
   reader.on("end", () => {
     writers.forEach((writer) => writer.end());
     console.log("Number of processed biblio records: " + reader.count);
-    console.log(
-      "papers / serials / total: " +
-        newspaperCounter +
-        " / " +
-        serialCounter +
-        " / " +
-        reader.count
-    );
-    console.log(
-      "Stats: " +
-        countNoNatLibMarc +
-        " missing control number, " +
-        countMatch +
-        " match, " +
-        countNoMatch +
-        " don't match."
-    );
+    console.log(statsToString());
+
     clearInterval(tick);
   });
 }
